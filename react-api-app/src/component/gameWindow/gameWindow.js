9a61b0466c5ff13c3c0ac2dde328df2861c495d0
Ghassan <g4ss4n@gmail.com>
2019-09-03 16:56:38 +0300

    Submit section

---
+++ react-api-app/src/component/gameWindow/gameWindow.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./gameWindow.css";
+
+import SubmitSection from "./submitSection/SubmitSection";
+
+const GameWindow = () => {
+  return (
+    <div id="gameWindow">
+      <SubmitSection />
+    </div>
+  );
+};
+
+export default GameWindow;
