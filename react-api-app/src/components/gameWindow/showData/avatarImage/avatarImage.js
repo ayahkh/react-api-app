343f1599b6fcabb850b5426dad5f0bdf07c86249
אולגה מולינס <OLGA@wlghs-MacBook-Air.local>
2019-09-04 12:29:19 +0300

    displays the info fetched from github on click

--- react-api-app/src/components/gameWindow/showData/avatarImage/avatarImage.js
+++ react-api-app/src/components/gameWindow/showData/avatarImage/avatarImage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./avatarImage.css";
 
-const AvatarImage=(props)=>{
-
-
+const AvatarImage = props => {
   return (
     <div>
-    <img src={props.url} >
+      <img src={props.url} />
     </div>
   );
-}
+};
 
 export default AvatarImage;
