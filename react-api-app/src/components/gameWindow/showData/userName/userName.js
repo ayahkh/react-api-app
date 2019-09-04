8995f16454a5cccbee0ddb63fa20f541d8dbcb2f
ayah <ayah_kh5@hotmail.com>
2019-09-04 11:31:51 +0300

    added two components for the avatar pic and the username (github)

---
+++ react-api-app/src/components/gameWindow/showData/userName/userName.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./userName.css";
+
+const UserName=(props)=>{
+
+  return (
+    <div>
+    <p> {props.name} </p>
+    </div>
+  );
+}
+
+export default UserName;
