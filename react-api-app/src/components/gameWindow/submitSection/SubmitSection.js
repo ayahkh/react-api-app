a0ae7117aa7e9e7ac070dda4ebe9cd04bce134c0
Ghassan <g4ss4n@gmail.com>
2019-09-05 14:56:44 +0300

    final final

--- react-api-app/src/components/gameWindow/submitSection/SubmitSection.js
+++ react-api-app/src/components/gameWindow/submitSection/SubmitSection.js
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
 import "./submitSection.css";
-import {selectTag} from '../main'
+import { selectTag } from "../main";
 import Submit from "./submit/Submit";
 import Answer from "./answer/Answer";
 
 const SubmitSection = () => {
   const [answer, setAnswer] = useState("");
+  const [result, setResult] = useState("");
 
-const checkSolution=()=>{
-  for(var i=0 ; i<selectTag.length ; i++)
-  {
-    if({answer}.answer === selectTag[i])
-    {
-      console.log("true answer");
-      
+  const checkSolution = () => {
+    if (true === selectTag.includes(answer)) {
+      setResult("✓ Right answer");
+    } else {
+      setResult("✗ wrong answer");
+      setAnswer("");
     }
-    
-  }
-}
-  
+  };
+
   return (
-    <div id="submitSection">
-      <Submit check={checkSolution} />
-      <Answer setAnswer={setAnswer} answer={answer} />
+    <div>
+      <section id="submitSection">
+        <Submit check={checkSolution} />
+        <Answer setAnswer={setAnswer} answer={answer} />
+      </section>
+      <section>
+        <h2>{result}</h2>
+      </section>
     </div>
   );
 };
